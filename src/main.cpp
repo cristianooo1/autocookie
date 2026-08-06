469d1fe3ee550a1def2a5db4ca9946848a7e15f4
cristianoo <cristi_iorga02@yahoo.com>
2026-08-06 12:45:32 +0300

    Added reset, reward, and terminal functions;

--- src/main.cpp
+++ src/main.cpp
@@ -170,6 +170,15 @@ struct Economy
     }
 };
 
+struct Observation
+{
+    double current_cookies{};
+    double all_time_cookies{};
+    std::array<int, Config::TOTAL_NR_BUILDINGS_AVAILABLE> buildings_owned{};
+    std::array<BuildingDefinition, Config::TOTAL_NR_BUILDINGS_AVAILABLE> buildings_definitions{};
+    double cps{};
+};
+
 struct Engine
 {
     const double dt{1.0};
@@ -177,12 +186,58 @@ struct Engine
     Economy economy;
     BuildingSystem buildingSystem;
 
+    double prev_progress_cookies{0.0};
+
     void step(const Action &action)
     {
         buildingSystem.update(state, action);
         state.time += dt;
         // events.update(state, dt);
         economy.update(state, dt);
+    }
+
+    void reset()
+    {
+        state.alltime_cookies = 0.0;
+        state.current_cookies = 0.0;
+        state.time = 0.0;
+        state.cps = 0.0;
+        for (int i = 0; i < Config::TOTAL_NR_BUILDINGS_AVAILABLE; i++)
+        {
+            state.buildingsOwned[i] = 0;
+        }
+
+        prev_progress_cookies = 0.0;
+    }
+
+    Observation get_observation()
+    {
+        return Observation{
+            .current_cookies = state.current_cookies,
+            .all_time_cookies = state.alltime_cookies,
+            .buildings_owned = state.buildingsOwned,
+            .buildings_definitions = Config::buildingsDefinitions,
+            .cps = state.cps,
+        };
+    }
+
+    double get_reward()
+    {
+        double reward = state.current_cookies - prev_progress_cookies;
+        prev_progress_cookies = state.current_cookies;
+        return reward;
+    }
+
+    bool is_terminal()
+    {
+        if (state.time == 100)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     std::tuple<double, double, double, double, int, int, int> queryState()
@@ -215,6 +270,7 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 int main(int argc, char *argv[])
 {
     Engine engine;
+    bool interactive_mode = false;
 
     Action clickCookie{.type = ActionType::ClickCookie};
     Action buyCursor{.type = ActionType::BuyBuilding, .buildingIndex = BuildingType::CURSOR};
@@ -222,146 +278,167 @@ int main(int argc, char *argv[])
     Action buyFarm{.type = ActionType::BuyBuilding, .buildingIndex = BuildingType::FARM};
     Action justWait{.type = ActionType::Wait};
 
-    // engine.step(buyCursor);
-    // engine.step(buyFarm);
-    // engine.step(clickCookie);
-    // engine.queryState();
-
-    // GLFW + Dear ImGUI
-    // https://github.com/ocornut/imgui/blob/master/examples/example_glfw_opengl3/main.cpp
-    if (!glfwInit())
+    if (interactive_mode != 0)
     {
-        std::cout << "GLFW Initialization failed" << "\n";
-        exit(EXIT_FAILURE);
+        engine.step(buyCursor);
+        engine.step(buyFarm);
+        engine.step(clickCookie);
+
+        const auto [currCookiess, alltimeCookiess, CPSs, time_from_startt, cursorss, grandmass, farmss] = engine.queryState();
+
+        std::cout << "current Cookies = " << currCookiess << "\n";
+        std::cout << "alltime Cookies = " << alltimeCookiess << "\n";
+        std::cout << "cps = " << CPSs << "\n";
+        std::cout << "time from start = " << time_from_startt << "\n";
+        std::cout << "cursros = " << cursorss << "\n";
+        std::cout << "grandmas = " << grandmass << "\n";
+        std::cout << "farms = " << farmss << "\n";
     }
-
-    // GL version + backend selects GLSL version
-    const char *glsl_version = nullptr;
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
-
-    GLFWwindow *window = glfwCreateWindow(1280, 720, "cookie test math model", nullptr, nullptr);
-    if (window == nullptr)
+    else
     {
-        exit(EXIT_FAILURE);
-    }
-
-    glfwSetKeyCallback(window, key_callback); // keyboard input
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // enable vsync
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-
-    // Dear ImGui style
-    ImGui::StyleColorsDark();
-    // ImGui::StyleColorsLight();
-
-    ImGui_ImplGlfw_InitForOpenGL(window, true); // pointer to the window created by GLFW
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    bool show_this_bool = true;
-    bool show_other_bool = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    while (!glfwWindowShouldClose(window))
-    {
-
-        glfwPollEvents();
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
+        // GLFW + Dear ImGUI
+        // https://github.com/ocornut/imgui/blob/master/examples/example_glfw_opengl3/main.cpp
+        if (!glfwInit())
         {
-
-            ImGui::Begin("cookie clicker stats");
-
-            ImGui::Text("statistics:"); // Display some text (you can use a format strings too)
-            // ImGui::Checkbox("Demo Window", &show_this_bool); // Edit bools storing our window open/close state
-            // ImGui::Checkbox("Another Window", &show_other_bool);
-
-            // ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-            // ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("COOKIE"))
-            {
-                engine.step(clickCookie);
-            }
-
-            ImGui::SameLine();
-
-            if (ImGui::Button("buy cursor"))
-            {
-                engine.step(buyCursor);
-            }
-
-            ImGui::SameLine();
-
-            if (ImGui::Button("buy grandma"))
-            {
-                engine.step(buyGrandma);
-            }
-
-            ImGui::SameLine();
-
-            if (ImGui::Button("buy farm"))
-            {
-                engine.step(buyFarm);
-            }
-
-            ImGui::SameLine();
-
-            if (ImGui::Button("wait"))
-            {
-                engine.step(justWait);
-            }
-
-            const auto [currCookiess, alltimeCookiess, CPSs, time_from_startt, cursorss, grandmass, farmss] = engine.queryState();
-
-            ImGui::Text("current Cookies = %.3f", currCookiess);
-
-            ImGui::Text("alltime Cookies = %.3f", alltimeCookiess);
-
-            ImGui::Text("CPS = %.3f", CPSs);
-
-            ImGui::Text("time from start = %.3f", time_from_startt);
-
-            ImGui::Text("cursors = %i", cursorss);
-
-            ImGui::Text("grandmas = %i", grandmass);
-
-            ImGui::Text("farms = %i", farmss);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
+            std::cout << "GLFW Initialization failed" << "\n";
+            exit(EXIT_FAILURE);
         }
 
-        // render
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        // GL version + backend selects GLSL version
+        const char *glsl_version = nullptr;
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 
-        glfwSwapBuffers(window);
+        GLFWwindow *window = glfwCreateWindow(1280, 720, "cookie test math model", nullptr, nullptr);
+        if (window == nullptr)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        glfwSetKeyCallback(window, key_callback); // keyboard input
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1); // enable vsync
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+
+        // Dear ImGui style
+        ImGui::StyleColorsDark();
+        // ImGui::StyleColorsLight();
+
+        ImGui_ImplGlfw_InitForOpenGL(window, true); // pointer to the window created by GLFW
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+        bool show_this_bool = true;
+        bool show_other_bool = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+        while (!glfwWindowShouldClose(window))
+        {
+
+            glfwPollEvents();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            {
+
+                ImGui::Begin("cookie clicker stats");
+
+                ImGui::Text("statistics:"); // Display some text (you can use a format strings too)
+                // ImGui::Checkbox("Demo Window", &show_this_bool); // Edit bools storing our window open/close state
+                // ImGui::Checkbox("Another Window", &show_other_bool);
+
+                // ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+                // ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("COOKIE"))
+                {
+                    engine.step(clickCookie);
+                }
+
+                ImGui::SameLine();
+
+                if (ImGui::Button("buy cursor"))
+                {
+                    engine.step(buyCursor);
+                }
+
+                ImGui::SameLine();
+
+                if (ImGui::Button("buy grandma"))
+                {
+                    engine.step(buyGrandma);
+                }
+
+                ImGui::SameLine();
+
+                if (ImGui::Button("buy farm"))
+                {
+                    engine.step(buyFarm);
+                }
+
+                ImGui::SameLine();
+
+                if (ImGui::Button("wait"))
+                {
+                    engine.step(justWait);
+                }
+
+                ImGui::SameLine();
+
+                if (ImGui::Button("reset"))
+                {
+                    engine.reset();
+                }
+
+                const auto [currCookiess, alltimeCookiess, CPSs, time_from_startt, cursorss, grandmass, farmss] = engine.queryState();
+
+                ImGui::Text("current Cookies = %.3f", currCookiess);
+
+                ImGui::Text("alltime Cookies = %.3f", alltimeCookiess);
+
+                ImGui::Text("CPS = %.3f", CPSs);
+
+                ImGui::Text("time from start = %.3f", time_from_startt);
+
+                ImGui::Text("cursors = %i", cursorss);
+
+                ImGui::Text("grandmas = %i", grandmass);
+
+                ImGui::Text("farms = %i", farmss);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                ImGui::End();
+            }
+
+            // render
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
+        }
+
+        // cleanup
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
-
-    // cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
 
     return 0;
 }
